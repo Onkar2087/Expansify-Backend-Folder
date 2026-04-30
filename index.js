66efeb00a94e0bed61ff6a7d9a84f7807cf473b5
Onkar2087 <114167896+Onkar2087@users.noreply.github.com>
2026-04-30 21:33:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,20 @@ let port = process.env.PORT || 8000
 let app = express()
 app.use(express.json())
 app.use(cookieParser())
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  "http://localhost:5173"
+];
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
